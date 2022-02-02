147e33ccf9ea453493b855c39d3a861fa937d2d9
Filippo Aluigi <fillo640@gmail.com>
2022-02-02 10:36:40 +0100

    completate 3 sezioni

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,61 @@
 <x-layout>
     <x-navbar>
     </x-navbar>
-    
+        {{-- header --}}
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <img src="./media/header.png" alt="cup of coffee" srcset="" class="imgheader">
+                   
+
+                </div>
+            </div>
+        </div>
+        {{-- section1 --}}
+        <div class="container-fluid">
+            <div class="row ">
+                <div class="col-12 col-md-6 d-flex align-items-center justify-content-center flex-column">
+                    <h2 class="my-5">Our history:</h2>
+                    <h6>We were born in 1234 in new york from a small family business, we have grown a lot to what we are today 20 employees around the world. Come and meet us. </h6>
+                </div>
+                <div class="col-12 col-md-6">
+                    <img src="./media/Nice to meet you.png" alt="" class="imgsotheader">
+                </div>
+            </div>
+        </div>
+        {{-- section2 --}}
+        <div class="container-fluid my-5">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-first">
+                    <img src="./media/Boston.png" alt="" srcset="" class="imgboston " >
+                </div>
+                <div class="col-12 col-md-6 my-5 d-flex align-items-center justify-content-center flex-column ">
+                    <h2 class="my-5">Our stores:</h2>
+                    <h6>You can find our stores in many cities including New York, Boston, Miami, Los Angeles, Washington. We are working to open new stores in Europe to expand our business.</h6>
+                </div>
+            </div>
+        </div>
+        {{-- section3 --}}
+        <div class="container-fluid">
+            <div class="row my-5 text-center">
+                <h2 class="text-center my-5">The founder of our agency: </h2>
+                <div class="col-12 col-md-4 my-5 text-center">
+                    <h3>Mario Rossi</h3>
+                    <img src="./media/persona1.jfif" alt="" class="img-fluid"srcset="">
+                    <h4>Co-founder Coffeidea</h4>
+                </div>
+                <div class="col-12 col-md-4 my-5">
+                    <h2>Gianluca Rossi</h2>
+                    <img src="./media/persona2.jfif" alt=""class="img-fluid" id="imm2" srcset=""> 
+                    <h3>Founder and CEO</h3>
+                </div>
+                <div class="col-12 col-md-4 my-5">
+                    <h3>Maria Rossi</h3>
+                    <img src="./media/persona 3.jfif" class="img-fluid" id="imm3"alt="" srcset="">
+                    <h4>Co-founder Coffeidea</h4>
+
+                </div>
+            </div>
+        </div>
+        
 </x-layout>
